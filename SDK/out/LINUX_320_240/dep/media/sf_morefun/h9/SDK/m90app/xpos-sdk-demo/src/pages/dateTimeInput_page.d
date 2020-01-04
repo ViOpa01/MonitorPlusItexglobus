28251e683350b205d509dbe1a59a04473bf1d1e1
Opeyemi Ajani <ajani.opeyemi@iisysgroup.com>
2020-01-04 13:27:36 +0100

    Update nibss 8583

--- SDK/out/LINUX_320_240/dep/media/sf_morefun/h9/SDK/m90app/xpos-sdk-demo/src/pages/dateTimeInput_page.d
+++
@@ -1,7 +0,0 @@
-/media/sf_morefun/h9/SDK/m90app/xpos-sdk-demo/../../out/LINUX_320_240/obj//media/sf_morefun/h9/SDK/m90app/xpos-sdk-demo/../../m90app/xpos-sdk-demo/src/pages/dateTimeInput_page.o: \
- /media/sf_morefun/h9/SDK/m90app/xpos-sdk-demo/../../m90app/xpos-sdk-demo/src/pages/dateTimeInput_page.c \
- /media/sf_morefun/h9/SDK/m90app/xpos-sdk-demo/../../m90app/xpos-sdk-demo/src/pages/dateTimeInput_page.h \
- /media/sf_morefun/h9/SDK/m90app/xpos-sdk-demo/../../libapi_xpos/inc/libapi_gui.h \
- /media/sf_morefun/h9/SDK/m90app/xpos-sdk-demo/../../libapi_xpos/inc/libapi_pub.h \
- /media/sf_morefun/h9/SDK/m90app/xpos-sdk-demo/../../libapi_xpos/inc/def.h \
- /media/sf_morefun/h9/SDK/m90app/xpos-sdk-demo/../../libapi_xpos/inc/libapi_system.h
