b27c0ea9562fc2acfeace4c563e1bcb48ac2b134
Jeremiah Adewole <jeremiah.adewole@iisysgroup.com>
2020-01-16 08:54:04 +0100

    Prep receipt redesign

--- SDK/m90app/xpos-sdk-demo/src/appInfo.h
+++ SDK/m90app/xpos-sdk-demo/src/appInfo.h
@@ -1,6 +1,13 @@
-#pragma once
+#ifndef APP_INFO__H
+#define APP_INFO__H
 
 // Declaring app information
 #define APP_NAME "TamsLite"
 #define APP_VER "V1.1.2"
 #define APP_MODEL "Morefun"
+
+
+
+
+
+#endif APP_INFO__H
